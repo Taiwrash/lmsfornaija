cd10c68157415fdaee5d9594cfa4d6f1cec2765d
Taiwrash <rasheedrtm1@gmail.com>
2020-11-22 01:00:59 +0100

    filter implementation a

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Loader from "./Loader";
 
 const Error = () => {
   return (
@@ -9,6 +10,7 @@ const Error = () => {
       <Link to="/" className="btn-error">
         Go Back Home
       </Link>
+      <Loader />
     </div>
   );
 };
--- src/pages/catalog/BookDetails.js
+++ src/pages/catalog/BookDetails.js
@@ -4,12 +4,25 @@ import { useParams, Link } from "react-router-dom";
 
 const BookDetails = () => {
   const { id } = useParams();
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    data.map((item) => {
+      return setValue(item);
+      // const newItems = item.filter((newItem) => item.id === id);
+    });
+  }, []);
+  const { title, category, desc } = value;
+  console.log(value);
 
   return (
     <div className="error-div">
-      <h2>
-        The id of the book you clicked have the id of {id} work in progress
-      </h2>
+      <h2>{title}</h2>
+      <h2>{desc}</h2>
+      <h2>{category}</h2>
+      <div>
+        <h2></h2>
+      </div>
 
       <Link to="/" className="btn-error">
         Go Back Home
