a2840fb1b473986feb069bc51db36634f05cec03
Taiwrash <rasheedrtm1@gmail.com>
2020-11-24 16:05:13 +0100

    comment component

--- src/pages/catalog/Book.js
+++ src/pages/catalog/Book.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import Reactions from "./Reactions";
 import { Link } from "react-router-dom";
+import data from "../../Data";
 const Book = ({ item }) => {
   const [text, setText] = useState(false);
+  const [value, setValue] = useState(null);
   return (
     <>
       {item.map((book) => {
         return (
           <div className="book-card" key={book.id}>
-            <img src="images/edu.png" alt="Book Cover" />
+            <img src={book.img} alt="Book Cover" />
             <div className="book-title">
               <p>{book.category}</p>
               <p>{book.title}</p>
--- src/pages/catalog/BookDetails.js
+++ src/pages/catalog/BookDetails.js
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from "react";
 import data from "../../Data";
 import { useParams, Link } from "react-router-dom";
+import Reactions from "./Reactions";
 
 const BookDetails = () => {
   const { id } = useParams();
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(data);
 
   useEffect(() => {
-    data.map((item) => {
-      return setValue(item);
+    data.find((item) => {
+      return item.id === +id && setValue(item);
+
       // const newItems = item.filter((newItem) => item.id === id);
     });
-  }, []);
-  const { title, category, desc } = value;
+  }, [id]);
+  const { title, category, desc, img } = value;
   console.log(value);
 
   return (
-    <div className="error-div">
-      <h2>{title}</h2>
-      <h2>{desc}</h2>
-      <h2>{category}</h2>
-      <div>
-        <h2></h2>
+    <div className="details">
+      <div className="book-card details-card">
+        <div className="img-div">
+          <img src={img} alt="Book Cover" />
+        </div>
+        <div className="book-title">
+          <p>Category: {category}</p>
+          <p>Book Title: {title}</p>
+        </div>
+        <div className="desc">
+          <p>{desc}</p>
+        </div>
+        <Reactions />
       </div>
-
-      <Link to="/" className="btn-error">
-        Go Back Home
-      </Link>
+      <div className="error-div">
+        <Link to="/" className="btn-error">
+          Go Back Home
+        </Link>
+      </div>
     </div>
   );
 };
